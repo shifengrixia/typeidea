5aa958e4bc18a7489872e450f7cdb84507601d8d
shifeng <yzy190120@163.com>
2019-12-19 11:10:53 +0800

    定制admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth import get_permission_codename
 from django.shortcuts import reverse
 from django.utils.html import format_html
 
+from .adminform import PostAdminForm
 from .models import Category, Tag, Post
+from typeidea.custom_site import custom_site
 
 
-@admin.register(Category)
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只展示当前用户分类"""
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc')
+    extra = 1
+    model = Post
+
+
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
+
+    inlines = [PostInline,]
 
     def post_count(self, obj):
         return obj.post_set.count()
@@ -20,20 +46,29 @@ class CategoryAdmin(admin.ModelAdmin):
         return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
 
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
 
-@admin.register(Post)
+
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
     list_display = (
         'title', 'category', 'status', 'created_time', 'operator'
     )
     list_display_links = []
 
-    list_filter = ('category',)
+    list_filter = (CategoryOwnerFilter,)
+    # filter_vertical = ('tag',)
+    filter_horizontal = ('tag',)
+
     search_fields = ('title', 'category__name')
 
     actions_on_top = True
@@ -41,19 +76,60 @@ class PostAdmin(admin.ModelAdmin):
 
     save_on_top = True
 
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    exclude = ('owner',)
+
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            )
+        }),
+        ('额外信息', {
+            'classes': ('collapse',),
+            'fields': ('tag',),
+        })
     )
 
+    # def has_add_permission(self, request):
+    #     opts = self.opts
+    #     codename = get_permission_codename('add', opts)
+    #     perm_code = '%s.%s' % (opts.app_label, codename)
+    #     resp = request.get()
+    #     if resp.status_code == 200:
+    #         return True
+    #     else:
+    #         return False
+
     def operator(self, obj):
-        return format_html('<a href"{}">编辑</a>', reverse('admin:blog_post_change', args=(obj.id,)))
+        return format_html('<a href="{}">编辑</a>', reverse('cus_admin:blog_post_change', args=(obj.id,)))
 
     operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+    class Media:
+        css = {
+            'all': ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css',),
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
