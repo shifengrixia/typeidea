accb645b182c5a1d0b62da9efba6c0c7659c7afe
shifeng <yzy190120@163.com>
2020-01-09 15:58:10 +0800

    访问数统计

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 # from django.http import HttpResponse
-from django.db.models import Q
+from django.core.cache import cache
+from django.db.models import Q, F
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
@@ -79,6 +82,30 @@ class PostDetailView(CommonViewMixin, DetailView):
         context = super(PostDetailView, self).get_context_data(**kwargs)
         context.update({'comment_form': CommentForm, 'comment_list': Comment.get_by_target(self.request.path), })
         return context
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.handel_visited()
+        return response
+
+    def handel_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        pv_key = 'pv:%s:%s' % (uid, self.request.path)
+        uv_key = 'uv:%s:%s:%s' % (uid, str(date.today()), self.request.path)
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1 * 60)
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(pv_key, 1, 24 * 60 * 60)
+        if increase_pv and increase_uv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1, uv=F('uv') + 1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1)
+        elif increase_uv:
+            Post.objects.filter(ps=self.object.id).update(uv=F('uv') + 1)
 
 
 class CategoryView(IndexView):
